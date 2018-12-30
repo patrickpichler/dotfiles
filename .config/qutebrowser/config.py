d67f8541c2d738370c5a502e05e7836632f1f4a7
Patrick Pichler <patri.pichler@gmail.com>
2018-12-30 20:56:37 +0100

    [qutebrowser] switch to dracula theme

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,1 +1,15 @@
+
+import dracula.draw
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    },
+    'font': {
+        'family': 'Menlo, Terminus, Monaco, Monospace',
+        'size': 10
+    }
+})
+
 c.editor.command = [ "kitty", "bash", "-c", "eval $EDITOR {file}"]
