b7aec6c23283ba893030b41d4ad6bc47318b4023
Patrick Pichler <patri.pichler@gmail.com>
2018-12-31 11:05:41 +0100

    [qutebrowser] use chrome UA for web.whatsapp.com

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -12,7 +12,10 @@ dracula.draw.blood(c, {
     }
 })
 
-c.editor.command = [ "kitty", "bash", "-c", "eval $EDITOR {file}"]
+c.editor.command = [ 'kitty', 'bash', '-c', 'eval $EDITOR {file}']
 
 c.auto_save.session = True
 c.auto_save.interval = 5000
+
+with config.pattern('*://web.whatsapp.com/') as p:
+    p.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
