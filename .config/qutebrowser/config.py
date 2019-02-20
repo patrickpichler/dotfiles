d1f7f409f1a90cb56d46c63f137f789dc09f6937
Patrick Pichler <pichler.patrick@protonmail.com>
2019-02-20 15:47:46 +0100

    [qutebrowser] add b1-b49 alias

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -17,5 +17,15 @@ c.editor.command = [ 'kitty', 'bash', '-c', 'eval $EDITOR {file}']
 c.auto_save.session = True
 c.auto_save.interval = 5000
 
+c.aliases = {
+        "w": "session-save", 
+        "q": "quit", 
+        "wq": "quit --save",
+        "b": "buffer"
+        }
+
+for i in range(1, 50):
+    c.aliases[f"b{i}"] = f"buffer {i}"
+
 with config.pattern('*://web.whatsapp.com/') as p:
     p.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
