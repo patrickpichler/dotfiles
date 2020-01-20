24184dbccfe7b7ef6d8d04412a69842d462104a0
Patrick Pichler <mail@patrickpichler.dev>
2020-01-20 20:30:27 +0100

    [qutebrowser] Switch searx instance to searx.be
    The searx.xyz instance was kinda slow, so the instance has been switched
    to searx.be for testing.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -38,8 +38,8 @@ config.bind('<Ctrl-n>', 'completion-item-focus --history next', mode='command')
 config.bind('<Ctrl-p>', 'completion-item-focus --history prev', mode='command')
 
 c.url.searchengines = {
-    'DEFAULT': 'https://searx.xyz/?language=en-US&q={}',
-    '!searchx': 'https://searx.xyz/?language=en-US&q={}',
+    'DEFAULT': 'https://searx.be/?language=en-US&q={}',
+    '!searchx': 'https://searx.be/?language=en-US&q={}',
     '!sp': 'https://startpage.com/do/search?q={}',
     '!ddg': 'https://duckduckgo.com/?q={}',
     '!gh': 'https://github.com/search?q={}',
