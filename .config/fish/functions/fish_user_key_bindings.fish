c38ef8259a716123efc88ec5b8d4afd524beda4c
Patrick Pichler <me@mail.patrickpichler.xyz>
2020-05-07 07:38:29 +0200

    [fish] Add alt + l keybinding for forward-char
    Using the arrow keys for completing is a bit annoying, as it forces you
    out of you hand position on the keyboard. There is now a keybinding
    alt+l which does the same as the right arrow key and is easier to reach.

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,4 +2,7 @@ function fish_user_key_bindings
   fzf_key_bindings
 
   bind --user \cx\ce edit_command_buffer
+
+  # use alt + l to move forward (less annoying than pressing right)
+  bind --user \el forward-char
 end
