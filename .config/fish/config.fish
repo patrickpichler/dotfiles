02046e04f40935213020cdf786e24eca713cf5ea
Patrick Pichler <me@mail.patrickpichler.xyz>
2020-05-08 07:26:41 +0200

    [fish] Move extension.fish to fish config folder

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ export EDITOR=nvim
 
 set MANPATH /usr/share/man ~/.local/share/man
 
-if test -f ~/.extension.fish; then
-  source ~/.extension.fish
+if test -f ~/.config/fish/extension.fish
+  source ~/.config/fish/extension.fish
 end
 
