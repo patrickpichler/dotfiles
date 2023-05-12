f0acd1cfeaa11516ff9b4d9ccb8b5e8974934a25
Patrick Pichler <patrick.pichler@dynatrace.com>
2023-05-12 08:05:57 +0200

    [nvim] disable distracting foldcolumn

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -18,7 +18,7 @@ vim.opt.smartindent = true
 vim.opt.termencoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 
-vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldcolumn = '0' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
