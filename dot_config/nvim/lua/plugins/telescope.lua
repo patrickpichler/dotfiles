e1dde020ae9d43eabe32a91211f173e6a9bec740
Patrick Pichler <patrick.pichler@dynatrace.com>
2023-05-16 10:50:29 +0200

    [nvim] add live_greps_args telescope plugin

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -9,12 +9,34 @@ return {
       { 'nvim-telescope/telescope-ui-select.nvim' },
       { 'debugloop/telescope-undo.nvim' },
       { 'rcarriga/nvim-notify' },
+      { 'nvim-telescope/telescope-live-grep-args.nvim' },
     },
 
     config = function(_, opts)
       local telescope = require('telescope')
+      local lga_actions = require("telescope-live-grep-args.actions")
 
-      telescope.setup(opts)
+      telescope.setup({
+        defaults = {
+          vimgrep_arguments = { 'rg', '--hidden', '--color=never',
+            '--no-heading', '--with-filename', '--line-number',
+            '--column', '--smart-case', '-g', '!.git', },
+        },
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true, -- enable/disable auto-quoting
+            -- define mappings, e.g.
+            mappings = {
+              -- extend mappings
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                ["<C-f>"] = lga_actions.quote_prompt({ postfix = " --fixed-strings" }),
+              },
+            },
+          }
+        }
+      })
       telescope.load_extension('ui-select')
       telescope.load_extension('undo')
       telescope.load_extension('notify')
@@ -32,21 +54,13 @@ return {
       {
         '<space>g',
         function()
-          require('telescope.builtin').live_grep()
+          require('telescope').extensions.live_grep_args.live_grep_args()
         end
       }
     },
 
     cmd = {
       "Telescope",
-    },
-
-    opts = {
-      defaults = {
-        vimgrep_arguments = { 'rg', '--hidden', '--color=never',
-          '--no-heading', '--with-filename', '--line-number',
-          '--column', '--smart-case', '-g', '!.git' },
-      },
     },
   },
 }
