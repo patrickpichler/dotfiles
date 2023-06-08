0a830414e0c032272d35f7d65b0c82455f6906d6
Patrick Pichler <git@patrickpichler.dev>
2023-06-08 23:29:26 +0200

    [hammerspoon] improve window selecting feature

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -4,22 +4,25 @@ spoon.SpoonInstall:andUse('EmmyLua')
 
 SUPER = { 'cmd', 'ctrl' }
 
+hs.hotkey.bind('cmd', 'h', function()
+end)
+
 hs.hotkey.bind(SUPER, 't', function()
   os.execute('open -n -b org.alacritty')
 end)
 
 hs.hotkey.bind(SUPER, 'h', function()
-  hs.window.filter.defaultCurrentSpace:focusWindowWest(nil,true,true)
+  hs.window.focusedWindow():focusWindowWest(nil, true, true)
 end)
 
 hs.hotkey.bind(SUPER, 'l', function()
-  hs.window.filter.defaultCurrentSpace:focusWindowEast(nil,true,true)
-end)
-
-hs.hotkey.bind(SUPER, 'j', function()
-  hs.window.filter.defaultCurrentSpace:focusWindowNorth(nil,true,true)
+  hs.window.focusedWindow():focusWindowEast(nil, true, true)
 end)
 
 hs.hotkey.bind(SUPER, 'k', function()
-  hs.window.filter.defaultCurrentSpace:focusWindowSouth(nil,true,true)
+  hs.window.focusedWindow():focusWindowNorth(nil, true, true)
+end)
+
+hs.hotkey.bind(SUPER, 'j', function()
+  hs.window.focusedWindow():focusWindowSouth(nil, true, true)
 end)
