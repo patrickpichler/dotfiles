4438dd077df4ceba9b26ac587feb2370d61c725b
Patrick Pichler <git@patrickpichler.dev>
2023-06-08 23:02:14 +0200

    [hammerspoon] add shortcuts for switching windows

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -7,3 +7,19 @@ SUPER = { 'cmd', 'ctrl' }
 hs.hotkey.bind(SUPER, 't', function()
   os.execute('open -n -b org.alacritty')
 end)
+
+hs.hotkey.bind(SUPER, 'h', function()
+  hs.window.filter.defaultCurrentSpace:focusWindowWest(nil,true,true)
+end)
+
+hs.hotkey.bind(SUPER, 'l', function()
+  hs.window.filter.defaultCurrentSpace:focusWindowEast(nil,true,true)
+end)
+
+hs.hotkey.bind(SUPER, 'j', function()
+  hs.window.filter.defaultCurrentSpace:focusWindowNorth(nil,true,true)
+end)
+
+hs.hotkey.bind(SUPER, 'k', function()
+  hs.window.filter.defaultCurrentSpace:focusWindowSouth(nil,true,true)
+end)
