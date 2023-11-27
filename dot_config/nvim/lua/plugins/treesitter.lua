a7275c12124919fcbd922d6d1caab8db1fea24d1
Patrick Pichler <git@patrickpichler.dev>
2023-11-27 15:36:08 +0100

    [nvim] use treesitter additional regex for highlighting
    This is require for the templ treesitter grammar to work.

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -37,7 +37,10 @@ return {
       return {
         ensure_installed = "all",
         ignore_install = { "phpdoc" },
-        highlight = { enable = true },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
         indent = { enable = true },
         autotag = { enable = true },
         textobjects = {
