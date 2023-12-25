5a503bf4f5c084cbb6128de1b28454d57cd57d35
Patrick Pichler <git@patrickpichler.dev>
2023-12-25 10:47:16 +0100

    [hammerspoon] remove unused hlkj bindings

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -8,21 +8,6 @@ hs.hotkey.bind(SUPER, 't', function()
   os.execute('open -n -b org.alacritty')
 end)
 
-hs.hotkey.bind(SUPER, 'h', function()
-  hs.window.focusedWindow():focusWindowWest(nil, true, true)
-end)
-
-hs.hotkey.bind(SUPER, 'l', function()
-  hs.window.focusedWindow():focusWindowEast(nil, true, true)
-end)
-
-hs.hotkey.bind(SUPER, 'k', function()
-  hs.window.focusedWindow():focusWindowNorth(nil, true, true)
-end)
-
-hs.hotkey.bind(SUPER, 'j', function()
-  hs.window.focusedWindow():focusWindowSouth(nil, true, true)
-end)
 local function fuzzyQuery(s, m)
   local s_index = 1
   local m_index = 1
