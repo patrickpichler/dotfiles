33a3008162ce634073b42c0ccd88615d12fd2377
Patrick Pichler <patrick@cast.ai>
2024-01-26 08:51:17 +0100

    [nvim] improve todo snippets

--- dot_config/nvim/snippets/all.lua
+++ dot_config/nvim/snippets/all.lua
@@ -1,4 +1,5 @@
 local ls = require("luasnip")
+
 -- some shorthands...
 local s = ls.snippet
 local sn = ls.snippet_node
@@ -22,17 +23,17 @@ local conds_expand = require("luasnip.extras.conditions.expand")
 
 _G.luasnip = {}
 _G.luasnip.vars = {
-  username = 'patrick.pichler',
+  username = "patrick.pichler",
 }
 
-local calculate_comment_string = require('Comment.ft').calculate
-local utils = require('Comment.utils')
+local calculate_comment_string = require("Comment.ft").calculate
+local utils = require("Comment.utils")
 
 --- Get the comment string {beg,end} table
 ---@param ctype integer 1 for `line`-comment and 2 for `block`-comment
 ---@return table comment_strings {begcstring, endcstring}
 local function get_cstring(ctype)
-  -- use the `Comments.nvim` API to fetch the comment string for the region (eq. '--%s' or '--[[%s]]' for `lua`)
+  -- use the `Comments.nvim` API to fetch the comment string for the region (eq. "--%s" or "--[[%s]]" for `lua`)
   local cstring = calculate_comment_string { ctype = ctype, range = utils.get_region() } or vim.bo.commentstring
   -- as we want only the strings themselves and not strings ready for using `format` we want to split the left and right side
   local left, right = utils.unwrap_cstr(cstring)
@@ -43,16 +44,16 @@ end
 --- Options for marks to be used in a TODO comment
 local marks = {
   function()
-    return fmt('{}', i(1, _G.luasnip.vars.username))
+    return fmt("({})", i(1, _G.luasnip.vars.username))
   end,
   function()
-    return fmt('{}{}', { i(1, os.date '%d-%m-%y'), i(2, ', ' .. _G.luasnip.vars.username) })
+    return fmt("({}{})", { i(1, os.date "%d-%m-%y"), i(2, ", " .. _G.luasnip.vars.username) })
   end,
   function()
-    return fmt('{}', i(1, os.date '%d-%m-%y'))
+    return fmt("({})", i(1, os.date "%d-%m-%y"))
   end,
   function()
-    return t ''
+    return t ""
   end,
 }
 
@@ -66,7 +67,7 @@ local function todo_snippet_nodes(aliases, opts)
   end
 
   -- format them into the actual snippet
-  local comment_node = fmta('<> <>(<>): <> <>', {
+  local comment_node = fmta("<> <><>: <> <>", {
     f(function()
       return get_cstring(opts.ctype)[1] -- get <comment-string[1]>
     end),
@@ -86,7 +87,7 @@ end
 ---@param opts table merged with the snippet opts table
 local function todo_snippet(context, aliases, opts)
   opts = opts or {}
-  aliases = type(aliases) == 'string' and { aliases } or
+  aliases = type(aliases) == "string" and { aliases } or
       aliases -- if we do not have aliases, be smart about the function parameters
   context = context or {}
   if not context.trig then
@@ -94,39 +95,39 @@ local function todo_snippet(context, aliases, opts)
   end
   opts.ctype = opts.ctype or
       1                                                   -- comment type can be passed in the `opts` table, but if it is not, we have to ensure, it is defined
-  local alias_string = table.concat(aliases, '|')         -- `choice_node` documentation
+  local alias_string = table.concat(aliases, "|")         -- `choice_node` documentation
   context.name = context.name or
-      (alias_string .. ' comment')                        -- generate the `name` of the snippet if not defined
+      (alias_string .. " comment")                        -- generate the `name` of the snippet if not defined
   context.dscr = context.dscr or
-      (alias_string .. ' comment with a signature-mark')  -- generate the `dscr` if not defined
+      (alias_string .. " comment with a signature-mark")  -- generate the `dscr` if not defined
   context.docstring = context.docstring or
-      (' {1:' .. alias_string .. '}: {3} <{2:mark}>{0} ') -- generate the `docstring` if not defined
+      (" {1:" .. alias_string .. "}: {3} <{2:mark}>{0} ") -- generate the `docstring` if not defined
   local comment_node = todo_snippet_nodes(aliases, opts)  -- nodes from the previously defined function for their generation
   return s(context, comment_node, opts)                   -- the final todo-snippet constructed from our parameters
 end
 
 local todo_snippet_specs = {
-  { { trig = 'todo' },  'TODO' },
-  { { trig = 'fix' },   { 'FIX', 'BUG', 'ISSUE', 'FIXIT' } },
-  { { trig = 'hack' },  'HACK' },
-  { { trig = 'warn' },  { 'WARN', 'WARNING', 'XXX' } },
-  { { trig = 'perf' },  { 'PERF', 'PERFORMANCE', 'OPTIM', 'OPTIMIZE' } },
-  { { trig = 'note' },  { 'NOTE', 'INFO' } },
-  { { trig = 'todob' }, 'TODO',                                         { ctype = 2 } },
-  { { trig = 'fixb' },  { 'FIX', 'BUG', 'ISSUE', 'FIXIT' },             { ctype = 2 } },
-  { { trig = 'hackb' }, 'HACK',                                         { ctype = 2 } },
-  { { trig = 'warnb' }, { 'WARN', 'WARNING', 'XXX' },                   { ctype = 2 } },
-  { { trig = 'perfb' }, { 'PERF', 'PERFORMANCE', 'OPTIM', 'OPTIMIZE' }, { ctype = 2 } },
-  { { trig = 'noteb' }, { 'NOTE', 'INFO' },                             { ctype = 2 } },
+  { { trig = "todo" },  "TODO" },
+  { { trig = "fix" },   { "FIX", "BUG", "ISSUE", "FIXIT" } },
+  { { trig = "hack" },  "HACK" },
+  { { trig = "warn" },  { "WARN", "WARNING", "XXX" } },
+  { { trig = "perf" },  { "PERF", "PERFORMANCE", "OPTIM", "OPTIMIZE" } },
+  { { trig = "note" },  { "NOTE", "INFO" } },
+  { { trig = "todob" }, "TODO",                                         { ctype = 2 } },
+  { { trig = "fixb" },  { "FIX", "BUG", "ISSUE", "FIXIT" },             { ctype = 2 } },
+  { { trig = "hackb" }, "HACK",                                         { ctype = 2 } },
+  { { trig = "warnb" }, { "WARN", "WARNING", "XXX" },                   { ctype = 2 } },
+  { { trig = "perfb" }, { "PERF", "PERFORMANCE", "OPTIM", "OPTIMIZE" }, { ctype = 2 } },
+  { { trig = "noteb" }, { "NOTE", "INFO" },                             { ctype = 2 } },
 }
 
 local auto_todo_snippet_specs = {
-  { { trig = '/todo' }, 'TODO' },
-  { { trig = '/fix' },  { 'FIX', 'BUG', 'ISSUE', 'FIXIT' } },
-  { { trig = '/hack' }, 'HACK' },
-  { { trig = '/warn' }, { 'WARN', 'WARNING', 'XXX' } },
-  { { trig = '/perf' }, { 'PERF', 'PERFORMANCE', 'OPTIM', 'OPTIMIZE' } },
-  { { trig = '/note' }, { 'NOTE', 'INFO' } },
+  { { trig = "/todo" }, "TODO" },
+  { { trig = "/fix" },  { "FIX", "BUG", "ISSUE", "FIXIT" } },
+  { { trig = "/hack" }, "HACK" },
+  { { trig = "/warn" }, { "WARN", "WARNING", "XXX" } },
+  { { trig = "/perf" }, { "PERF", "PERFORMANCE", "OPTIM", "OPTIMIZE" } },
+  { { trig = "/note" }, { "NOTE", "INFO" } },
 }
 
 local snippets = {}
