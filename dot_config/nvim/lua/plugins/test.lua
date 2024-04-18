7714d064166c043650314c02e46607ce5d52a5d1
Patrick Pichler <git@patrickpichler.dev>
2024-04-18 07:28:54 +0200

    [nvim] add missing neotest dependency

--- dot_config/nvim/lua/plugins/test.lua
+++ dot_config/nvim/lua/plugins/test.lua
@@ -2,10 +2,11 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
-      "nvim-neotest/neotest-go"
+      "nvim-neotest/neotest-go",
     },
 
     opts = {},
