4d089b56455ad5c3ae6b7ce17bc9290b18191e1e
Patrick Pichler <patrick@cast.ai>
2024-05-07 10:53:47 +0200

    [wezterm] adjust colorscheme based on macos

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -22,8 +22,25 @@ end
 connection_config.infuse(config)
 
 config.term = 'wezterm'
-
 config.color_scheme = 'tokyonight_night'
+
+local function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    return "tokyonight_night"
+  else
+    return "tokyonight_day"
+  end
+end
+
+wezterm.on("window-config-reloaded", function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = scheme_for_appearance(appearance)
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
+end)
 
 config.tab_bar_at_bottom = true
 
