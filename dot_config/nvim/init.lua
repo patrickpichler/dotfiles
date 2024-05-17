fd1efd15de14d6eaebf2c163910e3cfeb3357127
Patrick Pichler <patrick@cast.ai>
2024-05-17 07:12:01 +0200

    [nvim] remove default dark background

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,8 +3,6 @@ require("config.commands")
 require("config.keymaps")
 require("config.options")
 
-vim.opt.background = "dark"
-
 require("config.lazy") {
   dev = {
     path = "~/development/projects"
