ce2c431843b8bb9d56cf1d580184f82ba4b01069
Patrick Pichler <patrick@cast.ai>
2024-05-31 07:28:06 +0200

    [nvim] use `harpoon add` instead of `harpoon append` command

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -449,8 +449,8 @@ return {
       vim.keymap.set("n", "<leader>hw", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
         { desc = "Open [h]arpoon [w]indow" })
 
-      vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end,
-        { desc = "[h]arpoon [a]ppend" })
+      vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end,
+        { desc = "[h]arpoon [a]dd" })
       vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
       vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
       vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
