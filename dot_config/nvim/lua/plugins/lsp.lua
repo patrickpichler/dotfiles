c38d2a3694ccb25808f71ec15506c2caf30c29a6
Patrick Pichler <patrick@cast.ai>
2024-07-10 13:36:00 +0200

    [nvim] fix trouble.nvim config

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -296,7 +296,6 @@ return {
     },
 
     opts = {
-      icons = true,
       fold_open = "v",      -- icon used for open folds
       fold_closed = ">",    -- icon used for closed folds
       indent_lines = false, -- add an indent guide below the fold icons
