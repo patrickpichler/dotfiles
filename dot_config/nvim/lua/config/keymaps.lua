2dfacc0f36580b1b455c892380c44a2a59586580
Patrick Pichler <git@patrickpichler.dev>
2024-11-11 08:39:03 +0100

    [nvim] add window resize keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,8 @@ vim.keymap.set({'n', 'v', 'x'}, '<C-c>', '<Esc>', { remap = true })
 -- Make C-P C-N behave the same as Up Down in command mode
 vim.keymap.set('c', '<C-p>', '<Up>', { remap = false })
 vim.keymap.set('c', '<C-n>', '<Down>', { remap = false })
+
+vim.keymap.set('n', '<M-h>', [[<cmd>vertical resize +5<cr>]], { remap = false })
+vim.keymap.set('n', '<M-l>', [[<cmd>vertical resize -5<cr>]], { remap = false })
+vim.keymap.set('n', '<M-k>', [[<cmd>horizontal resize +5<cr>]], { remap = false })
+vim.keymap.set('n', '<M-j>', [[<cmd>horizontal resize -5<cr>]], { remap = false })
