984fb7c871a42fbee3395c057a3370ede2d7900d
Patrick Pichler <patrick@cast.ai>
2025-03-31 08:47:26 +0200

    [nvim] enable virtual line diagnostics

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,6 +3,7 @@ require("config.commands")
 require("config.keymaps")
 require("config.options")
 require("config.autocolor")
+require("config.diagnostics")
 
 require("config.lazy") {
   dev = {
---
+++ dot_config/nvim/lua/config/diagnostics.lua
@@ -0,0 +1,10 @@
+vim.diagnostic.config({
+  -- Use the default configuration
+  virtual_lines = true
+
+  -- Alternatively, customize specific options
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+})
