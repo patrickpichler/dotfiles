97fb70310172dd2b27671626dc886167d7105fca
Patrick Pichler <patrick@miggo.io>
2025-09-02 16:34:04 +0200

    [nvim] add rest.nvim

--- dot_config/nvim/lua/plugins/misc.lua
+++ dot_config/nvim/lua/plugins/misc.lua
@@ -47,4 +47,15 @@ return {
       },
     },
   },
+
+  {
+    "rest-nvim/rest.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "http")
+      end,
+    }
+  }
 }
