1602d98f54b7273e7e210a3a4974e5dd727c26e9
Patrick Pichler <patrick@cast.ai>
2026-01-16 19:03:15 +0100

    [nvim] replace codecompanion.nvim with opencode.nvim

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,5 +1,6 @@
 return {
   {
+    enabled = false,
     "olimorris/codecompanion.nvim",
 
     dependencies = {
@@ -55,6 +56,32 @@ return {
 
     keys = {
       { "<leader>at", ":CodeCompanionChat toggle<CR>", desc = "Toggle CodeCompanion Chat" },
+    },
+  },
+  {
+    "NickvanDyke/opencode.nvim",
+    dependencies = {
+      -- Recommended for `ask()` and `select()`.
+      -- Required for `snacks` provider.
+      ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+      { "folke/snacks.nvim" },
+    },
+    config = function()
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {
+        -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+      }
+
+      -- Required for `opts.events.reload`.
+      vim.o.autoread = true
+    end,
+    keys = {
+      { "<leader>a",  nil,                                                                  desc = "AI/OpenCode" },
+      { "<leader>ac", function() require("opencode").ask("@this: ", { submit = true }) end, desc = "Ask" },
+      { "<leader>ax", function() require("opencode").select() end,                          desc = "Execute opencode action..." },
+      { "<leader>af", function() require("opencode").toggle() end,                          desc = "Toggle" },
+      { "<leader>ao", function() require("opencode").operator("@this ") end,                desc = "Add range" },
+      { "<leader>ao", function() return require("opencode").operator("@this ") .. "_" end,  desc = "Add line" },
     },
   },
 }
