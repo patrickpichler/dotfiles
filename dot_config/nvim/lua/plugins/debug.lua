69f52f7066d818642015d0519b679eec36c989f8
Patrick Pichler <git@patrickpichler.dev>
2026-04-02 21:47:15 +0200

    [nvim] add debug config

---
+++ dot_config/nvim/lua/plugins/debug.lua
@@ -0,0 +1,72 @@
+return {
+  { "rcarriga/nvim-dap-ui", enabled = false },
+  {
+    "MironPascalCaseFan/debugmaster.nvim",
+    -- osv is needed if you want to debug neovim lua code. Also can be used
+    -- as a way to quickly test-drive the plugin without configuring debug adapters
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "jbyuki/one-small-step-for-vimkind",
+    },
+    config = function()
+      local dm = require("debugmaster")
+      -- make sure you don't have any other keymaps that starts with "<leader>d" to avoid delay
+      -- Alternative keybindings to "<leader>d" could be: "<leader>m", "<leader>;"
+      vim.keymap.set({ "n", "v" }, "<leader>d", dm.mode.toggle, { nowait = true })
+      -- If you want to disable debug mode in addition to leader+d using the Escape key:
+      -- vim.keymap.set("n", "<Esc>", dm.mode.disable)
+      -- This might be unwanted if you already use Esc for ":noh"
+      vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+      dm.plugins.osv_integration.enabled = true -- needed if you want to debug neovim lua code
+      local dap = require("dap")
+      -- Configure your debug adapters here
+      -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
+
+      dap.adapters.delve = function(callback, config)
+        if config.mode == 'remote' and config.request == 'attach' then
+          callback({
+            type = 'server',
+            host = config.host or '127.0.0.1',
+            port = config.port or '38697'
+          })
+        else
+          callback({
+            type = 'server',
+            port = '${port}',
+            executable = {
+              command = 'dlv',
+              args = { 'dap', '-l', '127.0.0.1:${port}', '--log', '--log-output=dap' },
+              detached = vim.fn.has("win32") == 0,
+            }
+          })
+        end
+      end
+
+      -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+      dap.configurations.go = {
+        {
+          type = "delve",
+          name = "Debug",
+          request = "launch",
+          program = "${file}"
+        },
+        {
+          type = "delve",
+          name = "Debug test", -- configuration for debugging test files
+          request = "launch",
+          mode = "test",
+          program = "${file}"
+        },
+        -- works with go.mod packages and sub packages
+        {
+          type = "delve",
+          name = "Debug test (go.mod)",
+          request = "launch",
+          mode = "test",
+          program = "./${relativeFileDirname}"
+        }
+      }
+    end
+  }
+}
--- dot_config/nvim/lua/plugins/statusline.lua
+++ dot_config/nvim/lua/plugins/statusline.lua
@@ -6,14 +6,29 @@ return {
       { "stevearc/overseer.nvim" },
     },
     opts = function()
+      local dmode_enabled = false
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "DebugModeChanged",
+        callback = function(args)
+          dmode_enabled = args.data.enabled
+        end
+      })
+
       return {
+        options = {
+          globalstatus = true,
+        },
         extensions = {
-          "neo-tree",
-          "fugitive",
           "trouble",
           "lazy",
         },
         sections = {
+          lualine_a = {
+            {
+              "mode",
+              fmt = function(str) return dmode_enabled and "DEBUG" or str end,
+            },
+          },
           lualine_c = {
             { "filename", file_status = true, path = 1 },
             { "overseer" }
