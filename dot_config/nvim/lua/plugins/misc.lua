4bf9d3f3053acd6b081d67c1efff066c787bb575
Patrick Pichler <git@patrickpichler.dev>
2026-05-04 08:16:46 +0200

    [nvim] add obsidian plugin

--- dot_config/nvim/lua/plugins/misc.lua
+++ dot_config/nvim/lua/plugins/misc.lua
@@ -45,4 +45,24 @@ return {
       },
     },
   },
+
+  {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*", -- use latest release, remove to use latest commit
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+      legacy_commands = false, -- this will be removed in 4.0.0
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/vaults/personal",
+        },
+        {
+          name = "work",
+          path = "~/vaults/work",
+        },
+      },
+    },
+  }
 }
