67711da79137ad31d92579e1da5491f9bd919f72
Patrick Pichler <patrick@cast.ai>
2026-05-16 19:34:17 +0200

    [nvim] upgrade blink.cmp

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -42,7 +42,3 @@ if vim.fn.executable('par') then
 end
 
 vim.opt.inccommand = "split"
-
--- HACK(patrick.pichler): this should fix issues with the statuscolumn.
--- See https://github.com/folke/snacks.nvim/issues/150
-vim.o.statuscolumn = [[%!v:lua.require'snacks.statuscolumn'.get()]]
